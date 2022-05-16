4329d6dab75abb9accacdcc9e8ed187683d5b518
Lucas Lo <lucaslo@creasant.com>
2022-05-16 17:52:31 -0400

    dev env

---
+++ webpack.config.js
@@ -0,0 +1,28 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+  mode: "development",
+  entry: {
+    index: "./src/index.js",
+    print: "./src/print.js",
+  },
+  output: {
+    filename: "[name].bundle.js",
+    path: path.resolve(__dirname, "dist"),
+    clean: true,
+    publicPath: "/",
+  },
+  devtool: "inline-source-map",
+  devServer: {
+    static: "./dist",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: "Webpack Demo",
+    }),
+  ],
+  optimization: {
+    runtimeChunk: "single",
+  },
+};
